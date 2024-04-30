84a238466daefc6474ac3977b7c7a3a7d02ba19f
ktatarnikovhiro <155456549+ktatarnikovhiro@users.noreply.github.com>
2024-04-30 11:42:43 +0200

    Grafana provisioning (#22)
    * [feature]provisioning fixes for grafana dashboards and users
    
    * [fix] removing mail
    
    * [feature] provisioning users
    
    ---------
    
    Co-authored-by: ktatarnikov <kostyantyn.tatarnikov@gmail.com>

--- integration/grafana_resources/provision.sh
+++ integration/grafana_resources/provision.sh
@@ -10,12 +10,25 @@ then
     exit 1
 fi
 
-# Add User
-echo "Creating user"
+# Add Users
+echo "Creating read_only user"
 curl -XPOST -u "${GRAFANA_USER}:${GRAFANA_PASSWORD}" \
     -H "Content-Type: application/json" \
     "${GRAFANA_URL}/api/admin/users" \
-    -d "@user.json"
+    -d "@read_only.json"
+
+# Add Users
+echo "Creating read_write user"
+USER_ID=`curl -XPOST -u "${GRAFANA_USER}:${GRAFANA_PASSWORD}" \
+    -H "Content-Type: application/json" \
+    "${GRAFANA_URL}/api/admin/users" \
+    -d "@read_write.json" | jq .id`
+
+echo "User created with id ${USER_ID}"
+curl -XPATCH -u "${GRAFANA_USER}:${GRAFANA_PASSWORD}" \
+    -H "Content-Type: application/json" \
+    "${GRAFANA_URL}/api/orgs/1/users/${USER_ID}" \
+    -d '{"role": "Editor"}'
 
 DASHBOARD_UIDS=`curl -u "${GRAFANA_USER}:${GRAFANA_PASSWORD}" "${GRAFANA_URL}/api/search" | jq ".[] | select( .type == \"dash-db\" ) | .uid " | tr -d '"'`
 for DASHBOARD_UID in ${DASHBOARD_UIDS}; do
@@ -24,6 +37,12 @@ for DASHBOARD_UID in ${DASHBOARD_UIDS}; do
         -H "Content-Type: application/json" \
         "${GRAFANA_URL}/api/access-control/dashboards/${DASHBOARD_UID}/builtInRoles/Viewer" \
         -d '{"permission": "View"}'
+
+    echo "Granting edit permission to dashboard ${DASHBOARD_UID}"
+    curl -XPOST -u "${GRAFANA_USER}:${GRAFANA_PASSWORD}" \
+        -H "Content-Type: application/json" \
+        "${GRAFANA_URL}/api/access-control/dashboards/${DASHBOARD_UID}/builtInRoles/Editor" \
+        -d '{"permission": "Edit"}'
 done
 
 echo "Done"
